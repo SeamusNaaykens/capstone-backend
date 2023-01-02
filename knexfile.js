bfb087077d6f81564797baa0e8c7274fb6a615fe
Seamus Naaykens <snaaykens@gmail.com>
2023-01-02 13:10:12 -0700

    changed knexfile to use environmental variables

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 module.exports = {
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
-    database: 'GrowLocal',
-    user: 'root',
-    password: 'rootroot',
-    charset: 'utf8'
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    charset: process.env.DB_CHARSET,
+    port: process.env.DB_PORT
   },
 };
